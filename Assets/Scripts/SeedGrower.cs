d9a9f99000088e65fb45b00cc77f65524511f7cf
Alex Coaton <alex.coaton@gmail.com>
2016-08-05 00:04:57 +0100

    Added garden plot, seeds, GrowableTomato Prefabs and SeedGrower script

---
+++ Assets/Scripts/SeedGrower.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeedGrower : MonoBehaviour {
+
+    GameObject seeds;
+
+    void Awake()
+    {
+        seeds = (GameObject)transform.FindChild("Seeds").gameObject;
+    }
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(growSeeds());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    IEnumerator growSeeds()
+    {
+        yield return new WaitForSeconds(3);
+
+        // Replace seeds with Growables
+
+        Destroy(seeds);
+    }
+}
